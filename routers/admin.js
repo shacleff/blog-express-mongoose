8e1cc6d743f0fb095d6d11d964fcf7513c2c0ad6
jianan <1072772483@qq.com>
2019-03-17 20:20:50 +0800

    分类的添加和删除

--- routers/admin.js
+++ routers/admin.js
@@ -27,26 +27,83 @@ router.get("/", function (req, res, next) {
 });
 
 /**
- * 用户管理
+ * 用户管理: 分页展示
  */
 router.get("/user", function (req, res, next) {
 
-    // 查询所有用户
-    User.find().then(function (users) {
 
-        res.render("admin/user_index", {
-            userInfo: req.userInfo,
-            users: users
+    // 玩家请求第几页
+    var page = parseInt(req.query.page || 1);
+
+    // 一页限制10条记录
+    var limit = 10;
+
+    // 当前一共几页
+    var pages = 0;
+
+    // 统计有几条记录
+    User.count().then(function (count) {
+        pages = Math.ceil(count/limit);
+
+        // 限制在合适的请求页数内
+        page = Math.min(page, pages);
+
+        // 最小1页
+        page = Math.max(page, 1);
+
+        // 当前请求页数跳过的数据条数
+        var skip = (page - 1) * limit;
+
+        User.find().sort({_id: -1}).limit(limit).skip(skip).then(function (users) {
+            res.render("admin/user_index", {
+                userInfo: req.userInfo,
+                users: users,
+                count: count,
+                pages: pages,
+                limit: limit,
+                page: page
+            });
         });
     });
 });
 
 /**
- * 分类首页
+ * 分类首页: 分页展示
  */
 router.get("/category", function (req, res, next) {
-    res.render("admin/category_index", {
-        userInfo: req.userInfo,
+
+    // 玩家请求第几页
+    var page = parseInt(req.query.page || 1);
+
+    // 一页限制10条记录
+    var limit = 10;
+
+    // 当前一共几页
+    var pages = 0;
+
+    // 统计有几条记录
+    Category.count().then(function (count) {
+       pages = Math.ceil(count/limit);
+
+       // 限制在合适的请求页数内
+       page = Math.min(page, pages);
+
+       // 最小1页
+       page = Math.max(page, 1);
+
+       // 当前请求页数跳过的数据条数
+       var skip = (page - 1) * limit;
+
+       Category.find().sort({_id: -1}).limit(limit).skip(skip).then(function (categories) {
+           res.render("admin/category_index", {
+               userInfo: req.userInfo,
+               categories: categories,
+               count: count,
+               pages: pages,
+               limit: limit,
+               page: page
+           });
+       });
     });
 });
 
@@ -101,9 +158,141 @@ router.post("/category/add", function (req, res, next) {
             url: "/admin/category"
         });
     });
+});
 
+/**
+ * 分类修改
+ */
+router.get("/category/edit", function (req, res) {
+    var id = req.query.id || "";
 
+    Category.findOne({
+        _id: id
+    }).then(function (category) {
+        if(!category){
+            res.render("admin/error", {
+                userInfo: req.userInfo,
+                message: "分类信息不存在"
+            });
+
+            return Promise.reject();
+        }
+
+        res.render("admin/category_edit", {
+            userInfo: req.userInfo,
+            category: category
+        });
+
+    });
+});
+
+/**
+ * 分类保存: 虽然和上面参数一样，但是这个是post，上面那个是get
+ */
+router.post("/category/edit", function (req, res, next) {
+
+    // 当前修改的
+    var id = req.query.id || '';
+
+    var name = req.body.name || "";
+
+    Category.findOne({
+        _id: id
+    }).then(function (category) {
+        if(!category){
+            res.render("admin/error", {
+                userInfo: req.userInfo,
+                message: "分类信息不存在"
+            });
+            return Promise.reject();  // 不再进入下面的then
+        }
+
+        // 名称没有修改
+        if(name == category.name){
+            res.render("admin/success", {
+                userInfo: req.userInfo,
+                message: "当前没做任何修改",
+                url: "/admin/category"
+            });
+
+            return Promise.reject();
+        }
+
+        return Category.findOne({
+
+            // 不等于当前id，名字还一样的
+            _id: {$ne: id},
+            name: name
+        });
+
+    }).then(function (sameCategory) {
+        if(sameCategory){
+            res.render("/admin/error", {
+                userInfo: req.userInfo,
+                message: "数据库中已经存在同名分类"
+            });
+            return Promise.reject();
+        }
+
+        /**
+         * 尝试更新数据, 这个add search很简单   update 难一点   del呢？
+         * 1.条件 2.要修改的值
+         */
+        return Category.update({
+            _id: id
+        }, {
+            name: name
+        });
+    }).then(function () {
+        res.render("admin/success", {
+            userInfo: req.userInfo,
+            message: "修改成功",
+            url: "/admin/category"
+        });
+    });
 
 });
 
+
+/**
+ * 分类删除
+ *   1.<a href="/admin/category/delete?id={{category._id.toString()}}">删除</a>   通过跳转的连接，因此，可以得到query的id
+ */
+router.get("/category/delete", function (req, res, next) {
+    var id = req.query.id || '';
+    Category.remove({
+        _id: id
+    }).then(function () {
+        res.render("admin/success", {
+            userInfo: req.userInfo,
+            message: "删除成功",
+            url: '/admin/category'
+        });
+    });
+});
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
