b186b65254674bc49836c55ec4755be3e6736876
jianan <1072772483@qq.com>
2019-03-19 00:00:51 +0800

    首页

--- routers/main.js
+++ routers/main.js
@@ -1,26 +1,34 @@
 var express = require("express");
 var router = express.Router();
 var Category = require("./../models/Category");
+var Content = require("./../models/Content");
 
 router.get("/", function (req, res, next) {
 
-    /**
-     * 读取数据库中的分类放前端展示
-     *   1.根据id排序
-     *      1 升序
-     *     -1 降序
-     */
+    var data = {
+        page: parseInt(req.query.page || 1),
+        limit: 2,
+        pages: 0,
+        categories: [],
+        count: 0,
+        userInfo: req.userInfo,
+    };
+
     Category.find().sort({_id: -1}).then(function (categories) {
-
-        console.log(categories);
-
-        /**
-         * 通过app.js中取得的cookies中的全局登陆状态数据，则把这个数据交给模板来处理,因此之前登陆过一次后，之后就直接登陆了              c
-         */
-        res.render("main/index", {
-            userInfo: req.userInfo,
-            categories: categories
+        data.categories = categories;
+        return Content.count();
+    }).then(function (count) {
+        data.count = count;
+        data.pages = Math.ceil(data.count / data.limit);
+        data.page = Math.min(data.page, data.pages);
+        data.page = Math.max(data.page, 1);
+        var skip = (data.page - 1) * data.limit;
+        return Content.find().limit(data.limit).skip(skip).populate(["category", "user"]).sort({
+            addTime: -1
         });
+    }).then(function (contents) {
+        data.contents = contents;
+        res.render("main/index", data);
     });
 });
 
