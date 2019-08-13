979a53994f52166e79d56ca665522864f07eba1a
jianan3 <jianan3@vipkid.com.cn>
2019-08-13 11:50:51 +0800

    blog mongodb增删改查

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,11 @@
 var mongoose = require("mongoose");
 
 var schema = new mongoose.Schema({
-
     // 分类名称
     name: {
         type: String,
         required: true
     },
-
 });
 
 module.exports = mongoose.model("Category", schema);
--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 /**
  * 用户表结构
- *   1.required 这个为true表示插入前必须检查，没传入则不会执行成功. 但是没报错，有点蛋疼
+ *   (1)required 这个为true表示插入前必须检查，没传入则不会执行成功. 但是没报错，有点蛋疼
+ *      因此服务器需要些上判断
  */
 var schema = new mongoose.Schema({
 
@@ -25,8 +26,8 @@ var schema = new mongoose.Schema({
 });
 
 /**
- * 1.导出可以操作的对象
- * 2.这个第一个参数将会是 mongodb中存储的表格的名字，而且不仅被变小写，而且被加了s，变为users
- * 3.好在导出后像操作对象一样对导出的这个对象操作来操作数据库,
+ * (1)导出可以操作的对象
+ * (2)这个第一个参数将会是 mongodb中存储的表格的名字，而且不仅被变小写，而且被加了s，变为users
+ * (3)好在导出后像操作对象一样对导出的这个对象操作来操作数据库,
  */
 module.exports = mongoose.model("User", schema);
